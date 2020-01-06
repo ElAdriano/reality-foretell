f82bb54545259914a2b3955ccd0db680f221c93b
Adrian Bączek <adrian@sharedmasters.com>
2020-01-06 23:41:51 +0100

    gui controller for DataForm.fxml template

--- src/GUI/Controllers/DataForm.java
+++ src/GUI/Controllers/DataForm.java
@@ -1,45 +1,117 @@
 package GUI.Controllers;
 
+import Management.Conditions;
+import Management.SchemeGenerator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
-import javafx.scene.control.TextArea;
-import javafx.scene.control.CheckBox;
-
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class DataForm {
-
-    private final String defaultInputValue = "";
+    @FXML
+    private Slider maxBudgetSlider;
+    @FXML
+    private Slider amountOfPrisonersSlider;
+    @FXML
+    private Slider priceOfPrisonWardSlider;
+    @FXML
+    private Slider maxPrisonersAmountSlider;
+    @FXML
+    private Slider priceOfSanitaryNookSlider;
+    @FXML
+    private Slider cameraRangeSlider;
+    @FXML
+    private Slider generationsAmountSlider;
 
     @FXML
-    private CheckBox cameraCheckbox;
+    private Label maxBudgetMonitor;
+    @FXML
+    private Label amountOfPrisonersMonitor;
+    @FXML
+    private Label priceOfPrisonWardMonitor;
+    @FXML
+    private Label maxPrisonersAmountMonitor;
+    @FXML
+    private Label priceOfSanitaryNookMonitor;
+    @FXML
+    private Label cameraRangeMonitor;
+    @FXML
+    private Label generationsAmountMonitor;
 
     @FXML
-    private TextArea cameraPriceInput;
-    @FXML
-    private TextArea windowPriceInput;
+    private Button nextPageButton;
 
-    private void setEnableForPriceInput(TextArea inputField){
-        inputField.setDisable(!inputField.isDisable());
-        if(inputField.isDisable()){
-            inputField.setText(defaultInputValue);
+    private DecimalFormat costFormat;
+    private DecimalFormat amountFormat;
+
+    public void initialize() {
+        costFormat = new DecimalFormat("0.00");
+        amountFormat = new DecimalFormat("0");
+
+        getEnteredDataFromCache();
+
+        setListener(maxBudgetSlider, maxBudgetMonitor, costFormat);
+        setListener(amountOfPrisonersSlider, amountOfPrisonersMonitor, amountFormat);
+        setListener(priceOfPrisonWardSlider, priceOfPrisonWardMonitor, costFormat);
+        setListener(maxPrisonersAmountSlider, maxPrisonersAmountMonitor, amountFormat);
+        setListener(priceOfSanitaryNookSlider, priceOfSanitaryNookMonitor, costFormat);
+        setListener(cameraRangeSlider, cameraRangeMonitor, costFormat);
+        setListener(generationsAmountSlider, generationsAmountMonitor, amountFormat);
+    }
+
+    private void getEnteredDataFromCache() {
+        setSliderValue(maxBudgetSlider, maxBudgetMonitor, SchemeGenerator.conditions.budget, costFormat);
+        setSliderValue(amountOfPrisonersSlider, amountOfPrisonersMonitor, SchemeGenerator.conditions.amountOfPrisoners, amountFormat);
+        setSliderValue(priceOfPrisonWardSlider, priceOfPrisonWardMonitor, SchemeGenerator.conditions.priceOfPrisonWard, costFormat);
+        setSliderValue(maxPrisonersAmountSlider, maxPrisonersAmountMonitor, SchemeGenerator.conditions.maxAmountOfPrisonersInPrisonWard, amountFormat);
+        setSliderValue(priceOfSanitaryNookSlider, priceOfSanitaryNookMonitor, SchemeGenerator.conditions.priceOfSanitaryNook, costFormat);
+        setSliderValue(cameraRangeSlider, cameraRangeMonitor, SchemeGenerator.conditions.cameraRange, costFormat);
+        setSliderValue(generationsAmountSlider, generationsAmountMonitor, SchemeGenerator.conditions.amountOfGenerations, amountFormat);
+    }
+
+    private void setSliderValue(Slider slider, Label monitor, double value, DecimalFormat textFormat) {
+        double valueToSetForSlider = value;
+        if (valueToSetForSlider == 0) {
+            valueToSetForSlider = (slider.getMin() + slider.getMax()) / 2;
         }
+        slider.setValue(valueToSetForSlider);
+        monitor.setText(textFormat.format(slider.getValue()));
+    }
+
+    private void setListener(Slider slider, Label monitor, DecimalFormat costFormat) {
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> monitor.setText(costFormat.format(newValue)));
     }
 
     @FXML
-    public void onCameraCheckboxClick(ActionEvent actionEvent) {
-        setEnableForPriceInput(cameraPriceInput);
+    private void onNextPageButtonClick(ActionEvent actionEvent) {
+        saveSlidersData();
+
+        Stage nextStage = null;
+        try {
+            nextStage = FXMLLoader.load(getClass().getResource("../Templates/DimensionsForm.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        Stage renderedStage = (Stage) nextPageButton.getScene().getWindow();
+        renderedStage.setScene(nextStage.getScene());
     }
 
-    @FXML
-    public void onWindowCheckboxClick(ActionEvent actionEvent) {
-        setEnableForPriceInput(windowPriceInput);
-    }
+    private void saveSlidersData() {
+        SchemeGenerator.conditions = new Conditions();
 
+        SchemeGenerator.conditions.budget = Math.round(100.0 * maxBudgetSlider.getValue()) / 100.0;
+        SchemeGenerator.conditions.amountOfPrisoners = (int) Math.round(amountOfPrisonersSlider.getValue());
+        SchemeGenerator.conditions.priceOfPrisonWard = Math.round(100.0 * priceOfPrisonWardSlider.getValue()) / 100.0;
+        SchemeGenerator.conditions.maxAmountOfPrisonersInPrisonWard = (int) Math.round(maxPrisonersAmountSlider.getValue());
+        SchemeGenerator.conditions.priceOfSanitaryNook = Math.round(100.0 * priceOfSanitaryNookSlider.getValue()) / 100.0;
+        SchemeGenerator.conditions.cameraRange = Math.round(100.0 * cameraRangeSlider.getValue()) / 100.0;
+        SchemeGenerator.conditions.amountOfGenerations = (int) Math.round(generationsAmountSlider.getValue());
+    }
 }
