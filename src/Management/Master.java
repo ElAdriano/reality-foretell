af0d1c735bc9ec734e021f972979f8ffab5b7eb2
Adrian Bączek <adrian@sharedmasters.com>
2020-01-07 23:09:15 +0100

    adding icon for app in left top corner

--- src/Management/Master.java
+++ src/Management/Master.java
@@ -2,15 +2,21 @@ package Management;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Master extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Stage stage = FXMLLoader.load(getClass().getResource("../GUI/Templates/StartMenu.fxml"));
+        Stage stage = FXMLLoader.load(getClass().getResource("/GUI/Templates/StartMenu.fxml"));
+
         primaryStage.setScene(stage.getScene());
         primaryStage.setTitle(stage.getTitle());
+        primaryStage.setResizable(false);
+        primaryStage.setWidth(606);
+        primaryStage.setHeight(450);
+        primaryStage.getIcons().add(new Image("/GUI/Backgrounds/RealityForetellIcon.png"));
         primaryStage.show();
     }
 
