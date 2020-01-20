8d3945887199cc751b4efbbcaff0dfb75cdeec89
Adrian Bączek <adrian@sharedmasters.com>
2020-01-20 11:22:30 +0100

    clean-up

--- ComplexModel.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-public class ComplexModel extends Model {
-
-    public ComplexModel(double startX, double sizeX, double startY, double sizeY) {
-        super(startX, sizeX, startY, sizeY);
-    }
-
-}
